cde1569079686af0a2e55bce1292171e0e416e96
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2023-12-12 21:36:44 -0500

    had the keys and values backwards, I guess

--- warehouse_bot.py
+++ warehouse_bot.py
@@ -5,7 +5,7 @@ gamma = 0.75
 
 actions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 location_to_state = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10, 'L':11}
-state_to_location = {s:l for s, l in location_to_state.items()}
+state_to_location = {l:s for s, l in location_to_state.items()}
 rewards = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
            [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0], 
            [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], 
