2256fe19eaf55474a1939d4a7f0d16601cdfdc1f
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2023-12-12 21:33:14 -0500

    replaced the '+' for an '='

--- warehouse_bot.py
+++ warehouse_bot.py
@@ -29,7 +29,7 @@ def get_route(starting, ending):
                       state = np.random.randint(0, 12)
                       playable = []
                       for ind in range(12):
-                                 if new_rewards[state, ind] !+ 0:
+                                 if new_rewards[state, ind] != 0:
                                             playable.append(ind)
                       next_state = np.random.choice(playable)
                       TD = new_rewards[state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state, ])] - Q[state, next_state]
